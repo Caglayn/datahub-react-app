9ffd91728a0d4fe0d4f734ca67889ae2436fad03
Caglayn <caglayankaya@hotmail.com.tr>
2022-07-17 13:15:24 +0300

    edit app.js

--- src/container/App.js
+++ src/container/App.js
@@ -1,7 +1,5 @@
 import QueryTool from "../components/QueryTool";
 import BasicTable from "../tables/BasicTable";
-import EditableTable from "../tables/EditableTable";
-import ModalEditTable from "../tables/ModalEditTable";
 import Navbar from "./Navbar";
 
 
