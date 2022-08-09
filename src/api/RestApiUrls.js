503af8e174e5725f97a4318088b9f482841d1401
Caglayn <caglayankaya@hotmail.com.tr>
2022-08-09 15:33:53 +0300

    urls edited

--- src/api/RestApiUrls.js
+++ src/api/RestApiUrls.js
@@ -1,6 +1,6 @@
-export const DATA_SERVICE = 'http://130.185.118.99:30091'; //'http://localhost:8091'
-export const DATA_TRANSFORM_SERVICE = 'http://130.185.118.99:30092'; // 'http://localhost:8092'
-export const USER_SERVICE = 'http://130.185.118.99:30093'; //'http://localhost:8093'
+export const DATA_SERVICE = 'http://130.185.118.99:30091'; // 'http://localhost:8091';
+export const DATA_TRANSFORM_SERVICE = 'http://130.185.118.99:30092'; //'http://localhost:8092';
+export const USER_SERVICE = 'http://130.185.118.99:30093'; //'http://localhost:8093';
 
 export const SIGNIN = '/api/v1/auth/signin';
 
